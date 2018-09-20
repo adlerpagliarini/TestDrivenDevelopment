431664bc4624d8c0e41a21de8d12f3e5400b9e2f
AdlerPagliarini <adlerpagliarini@gmail.com>
2018-09-20 00:18:41 -0300

    Chapter05

---
+++ TestDrivenDevelopment.Tests/Capitulo-05/MaiorPrecoTest.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestDrivenDevelopment;
+
+namespace TestDrivenDevelopment.Tests
+{
+    [TestFixture]
+    public class MaiorPrecoTest {
+
+        [Test]
+        public void DeveRetornarZeroSeCarrinhoVazio()
+        {
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+            double valor = carrinho.ItemDeMaiorValor(carrinho);
+            Assert.AreEqual(0.0, valor, 0.0001);
+        }
+
+        [Test]
+        public void DeveRetornarValorDoItemSeCarrinhoCom1Elemento()
+        {
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+            carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            double valor = carrinho.ItemDeMaiorValor(carrinho);
+            Assert.AreEqual(900.0, valor, 0.0001);
+        }
+        [Test]
+        public void DeveRetornarValorDoItemSeCarrinhoComNElementos()
+        {
+            CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+            carrinho.Adiciona(new Item("Geladeira", 1, 900.0));
+            carrinho.Adiciona(new Item("Notebook", 3, 1950.0));
+            carrinho.Adiciona(new Item("Fogão", 2, 950.0));
+            double valor = carrinho.ItemDeMaiorValor(carrinho);
+            Assert.AreEqual(1950.0, valor, 0.0001);
+        }
+
+    }
+}
---
+++ TestDrivenDevelopment/Capitulo-05/CarrinhoDeCompras.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestDrivenDevelopment
+{
+    public class CarrinhoDeCompras
+    {
+        public IList<Item> Itens { get; private set; }
+
+        public CarrinhoDeCompras()
+        {
+            this.Itens = new List<Item>();
+        }
+
+        public void Adiciona(Item item)
+        {
+            this.Itens.Add(item);
+        }
+
+        public double ItemDeMaiorValor(CarrinhoDeCompras carrinho)
+        {
+            double? valorItem = carrinho.Itens.OrderByDescending(i => i.ValorUnitario).FirstOrDefault()?.ValorUnitario;
+            return valorItem.GetValueOrDefault();
+        }
+    }
+}
